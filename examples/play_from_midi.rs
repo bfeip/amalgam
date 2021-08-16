cbf622327f93b95f862245d3d984bb9441b76a5a
Zachary Harvie <moser.zh25@gmail.com>
2021-08-15 17:58:32 -0700

    Fix sample

--- examples/play_from_midi.rs
+++ examples/play_from_midi.rs
@@ -5,37 +5,50 @@ use std::fs;
 use std::path::PathBuf;
 use std::sync::{Arc, Mutex};
 
-use synth::module::common::{IntoMutexPtr, SignalOutputModule, MutexPtr};
+use synth::module::common::*;
 use synth::module;
-use synth::note::Note;
+
+struct OscillatorFrequencyOverride {
+    freqs: Vec<Option<f32>>
+}
+
+impl OscillatorFrequencyOverride {
+    fn new(freqs: Vec<Option<f32>>) -> Self {
+        Self { freqs }
+    }
+
+    fn set(&mut self, freqs: Vec<Option<f32>>) {
+        self.freqs = freqs;
+    }
+}
+
+impl OptionalSignalOutputModule for OscillatorFrequencyOverride {
+    fn fill_optional_output_buffer(&mut self, buffer: &mut[Option<f32>], _output_info: &OutputInfo) {
+        let buffer_len = buffer.len();
+        assert!(buffer_len == self.freqs.len(), "Mismatched buffers");
+        for (buffer_val, &freq) in buffer.iter_mut().zip(self.freqs.iter()) {
+            *buffer_val = freq;
+        }
+    }
+}
 
 #[derive(Clone)]
 struct ExampleVoice {
     osc: MutexPtr<module::oscillator::Oscillator>,
-    active: bool
+    freq_override: MutexPtr<OscillatorFrequencyOverride>
 }
 
 impl ExampleVoice {
     fn new() -> Self {
-        let osc = Arc::new(Mutex::new(module::oscillator::Oscillator::new()));
-        Self { osc, active: false }
+        let mut unmutexed_osc = module::oscillator::Oscillator::new();
+        let freq_override = Arc::new(Mutex::new(OscillatorFrequencyOverride::new(Vec::new())));
+        unmutexed_osc.set_frequency_override_input(freq_override.clone());
+        let osc = Arc::new(Mutex::new(unmutexed_osc));
+        Self { osc, freq_override }
     }
 }
 
 impl module::voice::Voice for ExampleVoice {
-    fn on_activate(&mut self, note: Note) {
-        self.osc.lock().expect("Osc lock is poisoned").set_frequency(note.to_freq());
-        self.active = true;
-    }
-
-    fn on_start_deactivate(&mut self) {
-        self.active = false;
-    }
-
-    fn fully_deactivated(&self) -> bool {
-        !self.active
-    }
-
     fn update(&mut self, reference: &Self) {
         let ref_osc = reference.osc.lock().expect("Reference Osc lock is poisoned");
         let mut osc = self.osc.lock().expect("Osc lock is poisoned");
@@ -46,8 +59,65 @@ impl module::voice::Voice for ExampleVoice {
         state.waveform = ref_state.waveform;
     }
 
-    fn get_end_module(&mut self) -> MutexPtr<dyn SignalOutputModule> {
-        self.osc.clone()
+    fn fill_output_for_note_intervals(
+        &mut self, sample_buffer: &mut [f32], intervals: &[module::voice::NoteInterval],
+        output_info: &module::common::OutputInfo
+    ) {
+        assert!(!intervals.is_empty(), "Tried to get output with no note intervals");
+        let buffer_len = sample_buffer.len();
+
+        // Get freq value for each sample
+        let mut freq_values = Vec::with_capacity(buffer_len);
+        let mut sample_counter = 0_usize;
+        for note_interval in intervals {
+            // Some checks that note intervals appear in the expected order and do not overlap one another
+            let is_correct_start_note = note_interval.start_sample.is_none() && sample_counter == 0;
+            let is_ordered = note_interval.start_sample.unwrap_or(sample_counter) >= sample_counter;
+            assert!(is_correct_start_note || is_ordered, "Overlapping intervals");
+            assert!(sample_counter < buffer_len, "Too many samples");
+
+            // If start sample is None that means the note started in a previous sample period and we enter this
+            // sample period with the note already playing
+            if note_interval.start_sample.is_some() {
+                while sample_counter != note_interval.start_sample.unwrap() {
+                    // Until the next note is played just push nothing
+                    freq_values.push(None);
+                    sample_counter += 1;
+                }
+            }
+
+            let note_freq = note_interval.note.to_freq();
+            let end_sample = note_interval.end_sample.unwrap_or(buffer_len);
+            while sample_counter != end_sample {
+                // Until the note is done playing, push the notes freq value
+                freq_values.push(Some(note_freq));
+                let note_freq = note_interval.note.to_freq();
+                let end_sample = note_interval.end_sample.unwrap_or(buffer_len);
+                while sample_counter != end_sample {
+                    // Until the note is done playing, push the notes freq value
+                    freq_values.push(Some(note_freq));
+                    sample_counter += 1;
+                }
+            }
+            while sample_counter < buffer_len {
+                freq_values.push(None);
+                sample_counter += 1;
+            }
+            {
+                // Lock and set values
+                let mut freq_override = self.freq_override.lock().expect("Lock is poisoned");
+                freq_override.set(freq_values.clone());
+            }
+
+            self.osc.lock().unwrap().fill_output_buffer(sample_buffer, output_info);
+            for (sample, &freq) in sample_buffer.iter_mut().zip(freq_values.iter()) {
+                if freq.is_none() {
+                    // Do not play if no note was active
+                    // This is kinda a simple envelope generator
+                    *sample = 0.0;
+                }
+            }
+        }
     }
 }
 
