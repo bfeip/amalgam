3a21210bb9af10731424b358255fa7bc7abc29e4
Zachary Harvie <moser.zh25@gmail.com>
2021-10-06 22:17:02 -0700

    Play tone example and remove main.rs

---
+++ examples/play_tone.rs
@@ -0,0 +1,34 @@
+use synth::error::*;
+
+fn main() -> SynthResult<()> {
+    let mut synth = match synth::Synth::new() {
+        Ok(synth) => synth,
+        Err(err) => {
+            let msg = format!("Failed to test full synth: {}", err);
+            return Err(SynthError::new(&msg));
+        }
+    };
+
+    let mut audio_output = match synth::output::AudioOutput::new(synth::output::OutputDeviceType::Cpal) {
+        Ok(audio_output) => audio_output,
+        Err(err) => {
+            let msg = format!("Failed to test output: failed to create audio output: {}", err);
+            return Err(SynthError::new(&msg));
+        }
+    };
+
+    let oscillator = Box::new(synth::module::oscillator::Oscillator::new());
+    synth.get_output_module_mut().set_audio_input(oscillator);
+    let synth_mutex_ptr = std::sync::Arc::new(std::sync::Mutex::new(synth));
+
+    if let Err(err) = synth::Synth::play(synth_mutex_ptr, &mut audio_output) {
+        let msg = format!("Failed to test full synth: {}", err);
+        return Err(SynthError::new(&msg));
+    }
+
+    // The audio is being played on a separate thread owned by cpal if I understand correctly
+    // so we need to sleep here to give it enough time to play a bit
+    std::thread::sleep(std::time::Duration::from_secs(1));
+
+    Ok(())
+}
