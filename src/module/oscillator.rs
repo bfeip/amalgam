31a54ce1680a3a40fd775b18f183337f77854c23
Zachary Harvie <moser.zh25@gmail.com>
2022-02-01 14:26:53 -0800

    Remove OscillatorState

--- src/module/oscillator.rs
+++ src/module/oscillator.rs
@@ -18,65 +18,50 @@ pub enum Waveform {
     Pulse
 }
 
-/// Contains the parameters that determine how an oscillator will sound
-#[derive(Debug, Clone)]
-pub struct OscillatorState {
-    /// Basic waveform that will be played
-    pub waveform: Waveform,
-    /// frequency in Hz that the wave will be played at
-    pub frequency: f32,
-    /// Width of the pulse. Only used for pulse waveforms. 50% is square, 0% and 100% are silent
-    pub pulse_width: f32,
-}
-
-impl OscillatorState {
-    /// Creates a basic sine wave oscillator state in C4 with a 50% pulse width
-    pub fn new() -> Self {
-        Self {
-            waveform: Waveform::Sine,
-            frequency: note::FREQ_C,
-            pulse_width: 0.5,
-        }
-    }
-}
-
 /// Represents a Oscillator capable of outputting values
 #[derive(Clone)]
 pub struct Oscillator {
-    /// Contains all the information needed to replicate the oscillator
-    state: OscillatorState,
+    /// Basic waveform that will be played
+    waveform: Waveform,
+    /// frequency in Hz that the wave will be played at
+    frequency: f32,
+    /// Width of the pulse. Only used for pulse waveforms. 50% is square, 0% and 100% are silent
+    pulse_width: f32,
     freq_override_input: MutexPtr<dyn OptionalSignalOutputModule>
 }
 
 impl Oscillator {
     /// Creates a basic sine wave oscillator stream with a default `OscillatorState`
     pub fn new() -> Self {
-        let state = OscillatorState::new();
-        let freq_input = Arc::new(Mutex::new(OptionalEmpty::new()));
-        Oscillator { state, freq_override_input: freq_input }
+        let waveform = Waveform::Sine;
+        let frequency = note::FREQ_C;
+        let pulse_width = 0.5;
+        let freq_override_input = Arc::new(Mutex::new(OptionalEmpty::new()));
+        Oscillator { waveform, frequency, pulse_width, freq_override_input }
     }
 
-    pub fn from_state(state: &OscillatorState) -> Self {
-        let freq_input = Arc::new(Mutex::new(OptionalEmpty::new()));
-        Oscillator { state: state.clone(), freq_override_input: freq_input }
+    pub fn set_waveform(&mut self, waveform: Waveform) {
+        self.waveform = waveform
     }
 
-    /// Retrieves a reference to the `OscillatorState`
-    pub fn get_state(&self) -> &OscillatorState {
-        &self.state
-    }
-
-    /// Retrieves a mutable reference to the `OscillatorState`
-    pub fn get_state_mut(&mut self) -> &mut OscillatorState {
-        &mut self.state
-    }
-
-    pub fn set_state(&mut self, state: &OscillatorState) {
-        self.state = state.clone();
+    pub fn get_waveform(&self) -> Waveform {
+        self.waveform
     }
 
     pub fn set_frequency(&mut self, freq: f32) {
-        self.state.frequency = freq
+        self.frequency = freq
+    }
+
+    pub fn get_frequency(&self) -> f32 {
+        self.frequency
+    }
+
+    pub fn set_pulse_width(&mut self, pulse_width: f32) {
+        self.pulse_width = pulse_width;
+    }
+
+    pub fn get_pulse_width(&self) -> f32 {
+        self.pulse_width
     }
 
     pub fn set_frequency_override_input(&mut self, override_input: MutexPtr<dyn OptionalSignalOutputModule>) {
@@ -128,7 +113,7 @@ impl Oscillator {
             let sample_rate = sample_rate as f32;
 
             let duration_offset = (clock_value * freq_value / sample_rate) % 1_f32;
-            buffer[i] = if duration_offset > self.state.pulse_width { 1_f32 } else { -1_f32 };
+            buffer[i] = if duration_offset > self.pulse_width { 1_f32 } else { -1_f32 };
         }
     }
 
@@ -139,14 +124,14 @@ impl Oscillator {
         let buffer_len = buffer.len();
 
         // Compute frequency per sample
-        let mut freq_values = vec![self.state.frequency; buffer_len];
+        let mut freq_values = vec![self.frequency; buffer_len];
         for (freq_value, &freq_override_elem) in freq_values.iter_mut().zip(freq_override_buffer.iter()) {
             if freq_override_elem.is_some() {
                 *freq_value = freq_override_elem.unwrap();
             }
         }
 
-        match self.state.waveform {
+        match self.waveform {
             Waveform::Sine     => self.fill_sine(buffer, clock_values, &freq_values, sample_rate),
             Waveform::Ramp     => self.fill_ramp(buffer, clock_values, &freq_values, sample_rate),
             Waveform::Saw      => self.fill_saw(buffer, clock_values, &freq_values, sample_rate),
